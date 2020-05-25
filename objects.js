09353e63129407d5be64e1c091ca51e12ea72bb5
Victor Romero <romeroa.victor@yahoo.com>
2020-05-25 22:26:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,9 @@
 var playlist={Prince:"song1"};
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName]=songTitle;
-return playlist;}
+return playlist;
+  }
+  function removeFromPlaylist(playlist, artistName){
+    delete playlist.artistName;
+      return playlist;
+  }
