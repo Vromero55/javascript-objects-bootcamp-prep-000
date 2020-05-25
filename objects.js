3475c1b313451b29e34f0349efb0aa5405fb8a68
Victor Romero <romeroa.victor@yahoo.com>
2020-05-25 22:19:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist={Prince:"song1"};
-function updatePlaylist(playlist, artistName, songTitle){
+/*function updatePlaylist(playlist, artistName, songTitle){
   return playlist.assign({},playlist,{[artistName]:songTitle});
-}
+}*/
